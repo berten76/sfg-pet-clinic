3bf02f8f160c063b93bfbd9b4ea6d67ab4eeddb4
christofer <christofer.berten@gmail.com>
2019-08-15 19:16:09 +0200

    Implemented config for serrvices, closes #21

--- pet-clinic-data/src/main/java/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,10 +1,12 @@
 package sfgpetclinic.services.map;
 
+import org.springframework.stereotype.Service;
 import sfgpetclinic.model.Owner;
 import sfgpetclinic.services.OwnerService;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends  AbstractMapClass<Owner, Long>
    implements OwnerService {
     @Override
--- pet-clinic-web/src/main/java/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/controllers/VetController.java
@@ -2,9 +2,16 @@ package sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sfgpetclinic.services.map.VetServiceMap;
 
 @Controller
 public class VetController {
+
+    private VetServiceMap vetServiceMap;
+
+    public VetController(VetServiceMap vetServiceMap) {
+        this.vetServiceMap = vetServiceMap;
+    }
 
     @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
     public String listVets(){
